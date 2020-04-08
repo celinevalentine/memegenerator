fca09d5e150600d30b18970e544765c455adde18
Celine Valentine <cyv419@gmail.com>
2020-04-07 21:30:05 -0700

    adding img fxn working

--- memes.js
+++ memes.js
@@ -1,39 +1,50 @@
 let memeForm = document.querySelector('#memeForm');
-let imgInput = document.querySelector('#url');
-let topTextInput = document.querySelector('input[name="topText"]');
-let btmTextInput = document.querySelector('input[name="btmText"]');
-let imgUrl = imgInput.value;
-let newImg = document.createElement("img");
-newImg.src = imgUrl;
 
-
-
-memeForm.addEventListener("submit" || "keypress", function(e) {
-    if (e.keycode === 13);
+memeForm.addEventListener('submit', function(e) {
+    // if (e.keycode === 13);
     e.preventDefault();
     // create image, get it from input sources then append it in the new div
+    let imgInput = document.querySelector('#url');
+    let imgUrl = imgInput.value;
+    let newImg = document.createElement("img");
+    newImg.setAttribute('src', imgUrl);
 
     newImg.setAttribute("height", "250px");
     newImg.setAttribute("width", "250px");
+    //add top and btm text to newImg
+    let topTextInput = document.querySelector('#topText');
+    let topText = topTextInput.value;
+    let btmTextInput = document.querySelector('#btmText');
+    let btmText = btmTextInput.value;
 
     let newDiv = document.createElement('div');
     let topTextDiv = document.createElement('div');
     let btmTextDiv = document.createElement('div');
+    topTextDiv.appendChild(topText);
+    btmTextDiv.appendChild(btmText);
 
-    newDiv.id = 'meme-results';
+    let removeButton = document.createElement("button");;
+    removeButton.innerText = "X";
+
     newDiv.appendChild(newImg);
     newDiv.appendChild(topTextDiv);
     newDiv.appendChild(btmTextDiv);
 
-    let removeButton = document.createElement("button");;
-    removeButton.innerText = "X";
-    newImg.appendChild(removeButton);
+    newDiv.appendChild(removeButton);
+
+    let canvas = document.querySelector('#meme-results');
+    canvas.appendChild(newDiv);
 
     memeForm.reset();
 });
 // delete meme 
-let memeList = document.querySelector('meme_results');
-memeList.addEventListener('click', function(e) {
+let imgInput = document.querySelector('#url');
+let imgUrl = imgInput.value;
+let newImg = document.createElement("img");
+newImg.setAttribute('src', imgUrl);
+
+newImg.addEventListener('click', function(e) {
+
     const clickedImg = e.target.tagName;
     if (clickedImg === 'button') {
         e.target.parentElement.remove();
